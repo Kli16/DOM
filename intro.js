45009593c291a5cd0848c3c3225b9ebd2b2b6473
Kevin Li <kevinli@dwightshrute.local>
2017-12-08 15:00:45 -0500

    did the remove tag thing

--- intro.js
+++ intro.js
@@ -1,15 +1,21 @@
 var list = document.getElementById('thelist');
 
 //button stuff
-var button = document.getElementById('b');
-button.addEventListener('click', addelement);
 var addelement = function(e){
      var li = document.createElement('li');
      li.innerHTML = 'newitem';
      list.appendChild(li);
 }
 
+var button = document.getElementById('b');
+button.addEventListener('click', addelement);
+
 //remove listelements
 var removeelement = function(e){
      this.remove();
+}
+
+var listelements = document.getElementsByTagName('li');
+for (li of listelements){
+  li.addEventListener('click', removeelement);
 }
