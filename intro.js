d4560ceb32bc0183eca7376e21c71549ded1124d
Kevin Li <kevinli@dwightshrute.home>
2017-12-08 17:35:00 -0500

    finished

--- intro.js
+++ intro.js
@@ -1,21 +1,40 @@
 var list = document.getElementById('thelist');
 
 //button stuff
+var button = document.getElementById('b');
+
 var addelement = function(e){
      var li = document.createElement('li');
      li.innerHTML = 'newitem';
      list.appendChild(li);
+     li.addEventListener('click', removeelement);
+     li.addEventListener('mouseover', changeheading);
+     li.addEventListener('mouseout', changeheadingback);
 }
 
-var button = document.getElementById('b');
 button.addEventListener('click', addelement);
 
+//heading changing
+var heading = document.getElementById('h');
+var origheading = heading.innerHTML;
+
+var changeheading = function(e){
+  heading.innerHTML = this.innerHTML;
+}
+
+var changeheadingback = function(e){
+  heading.innerHTML = origheading;
+}
+
 //remove listelements
+var listelements = document.getElementsByTagName('li');
+
 var removeelement = function(e){
      this.remove();
 }
 
-var listelements = document.getElementsByTagName('li');
 for (li of listelements){
   li.addEventListener('click', removeelement);
+  li.addEventListener('mouseover', changeheading);
+  li.addEventListener('mouseout', changeheadingback);
 }
